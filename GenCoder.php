520aee2133f5f21966acd6c0797f8d99dd7f45f0
artur <homoastricus2011@gmail.com>
2020-07-14 18:42:14 +0300

    changed key_symbols, changed generate_key method, chane random_int to mt_rand

--- GenCoder.php
+++ GenCoder.php
@@ -9,7 +9,7 @@ mb_http_input('UTF-8');
 mb_regex_encoding('UTF-8');
 mb_internal_encoding("UTF-8");
 
-Class GenCoder
+class GenCoder
 {
     public $message;
     public $salt;
@@ -19,29 +19,30 @@ Class GenCoder
     private $salt_length = 8;
     const hash_length = 32;
 
-    private $key_size_types = [
-        '64000',
-        '256000',
-        '1000000',
-        '4000000',
-        '16000000',
+    private $key_min = 64;
+    private $key_max = 100;
+
+    private $key_symbols = [
+        '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '0', '1',
+        '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '?', '!',
+        '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+        'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+        'X', 'Y', 'Z', '^', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+        'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '='
     ];
 
     /**
      * GenCoder constructor.
-     * @param $key_size
      * @param string $salt
      * @throws Exception
      */
-    public function __construct($key_size, $salt = "")
+    public function __construct($salt = "")
     {
-        if(!in_array($key_size, $this->key_size_types)){
-            $key_size = $this->key_size_types[0];
-        }
-        if(mb_strlen($salt)<8){
+        if (mb_strlen($salt) < 8) {
             $this->salt = $this->randomSalt($this->salt_length);
         }
-        $this->key_size = $key_size;
+        $this->key_size = mt_rand($this->key_min, $this->key_max);
         $this->salt = $salt;
     }
 
@@ -50,7 +51,8 @@ Class GenCoder
      * @param $salt
      * @return string
      */
-    private function user_hashcode($pass, $salt){
+    private function user_hashcode($pass, $salt)
+    {
         return md5($pass . $salt);
     }
 
@@ -58,7 +60,8 @@ Class GenCoder
      * @param $string
      * @return array
      */
-    private function str_splitUTF8 ($string) {
+    private function str_splitUTF8($string)
+    {
         return preg_split('//u', $string, null, PREG_SPLIT_NO_EMPTY);
     }
 
@@ -66,7 +69,8 @@ Class GenCoder
      * @param $user1_pass
      * @return string
      */
-    public function sender_hashcode($user1_pass){
+    public function sender_hashcode($user1_pass)
+    {
         return $user_code_1 = $this->user_hashcode($user1_pass, $this->salt);
     }
 
@@ -74,16 +78,18 @@ Class GenCoder
      * @param $user2_pass
      * @return string
      */
-    public function receiver_hashcode($user2_pass){
+    public function receiver_hashcode($user2_pass)
+    {
         return $user_code_2 = $this->user_hashcode($user2_pass, $this->salt);
     }
 
     /**
-     * @return string
-     * @throws Exception
+     * @param $length
+     * @return false|string
      */
-    private function randomSalt($length){
-        return substr(md5(random_bytes(16)), 0, $length);
+    private function randomSalt($length)
+    {
+        return substr(md5(uniqid().time()), 0, $length);
     }
 
     /**
@@ -91,7 +97,8 @@ Class GenCoder
      * @param $salt
      * @return string
      */
-    private function attachKey($message, $salt){
+    private function attachKey($message, $salt)
+    {
         return md5(hash('sha512', $message . time() . $salt) . hash('sha512', $salt));
     }
 
@@ -101,7 +108,8 @@ Class GenCoder
      * @param $attach_key
      * @return string
      */
-    private function pathKeySignature($user_code_1, $user_code_2, $attach_key){
+    private function pathKeySignature($user_code_1, $user_code_2, $attach_key)
+    {
         return hash('sha512', $user_code_1 . $attach_key . $user_code_2);
     }
 
@@ -109,8 +117,9 @@ Class GenCoder
      * @param $dec
      * @return false|string
      */
-    private function mb_chr($dec) {
-        if(function_exists("mb_chr")){
+    private function mb_chr($dec)
+    {
+        if (function_exists("mb_chr")) {
             return mb_chr($dec, "UTF-8");
         } else {
             if ($dec < 128) {
@@ -131,41 +140,39 @@ Class GenCoder
      * @param $char
      * @return bool|false|int
      */
-    private function mb_ord($char){
+    private function mb_ord($char)
+    {
         $encoding = 'UTF-8';
         if (!function_exists('mb_ord')) {
-                $c = $char;
-                $index = 0;
-                $len = strlen($c);
-                $bytes = 0;
+            $c = $char;
+            $index = 0;
+            $len = strlen($c);
+            $bytes = 0;
 
-                if ($index >= $len)
-                    return false;
-
-                $h = ord($c{$index});
-
-                if ($h <= 0x7F) {
-                    $bytes = 1;
-                    return $h;
-                }
-                else if ($h < 0xC2)
-                    return false;
-                else if ($h <= 0xDF && $index < $len - 1) {
-                    $bytes = 2;
-                    return ($h & 0x1F) <<  6 | (ord($c{$index + 1}) & 0x3F);
-                }
-                else if ($h <= 0xEF && $index < $len - 2) {
-                    $bytes = 3;
-                    return ($h & 0x0F) << 12 | (ord($c{$index + 1}) & 0x3F) << 6
-                        | (ord($c{$index + 2}) & 0x3F);
-                }
-                else if ($h <= 0xF4 && $index < $len - 3) {
-                    $bytes = 4;
-                    return ($h & 0x0F) << 18 | (ord($c{$index + 1}) & 0x3F) << 12
-                        | (ord($c{$index + 2}) & 0x3F) << 6
-                        | (ord($c{$index + 3}) & 0x3F);
-                }
+            if ($index >= $len)
                 return false;
+
+            $h = ord($c{$index});
+
+            if ($h <= 0x7F) {
+                $bytes = 1;
+                return $h;
+            } else if ($h < 0xC2)
+                return false;
+            else if ($h <= 0xDF && $index < $len - 1) {
+                $bytes = 2;
+                return ($h & 0x1F) << 6 | (ord($c{$index + 1}) & 0x3F);
+            } else if ($h <= 0xEF && $index < $len - 2) {
+                $bytes = 3;
+                return ($h & 0x0F) << 12 | (ord($c{$index + 1}) & 0x3F) << 6
+                    | (ord($c{$index + 2}) & 0x3F);
+            } else if ($h <= 0xF4 && $index < $len - 3) {
+                $bytes = 4;
+                return ($h & 0x0F) << 18 | (ord($c{$index + 1}) & 0x3F) << 12
+                    | (ord($c{$index + 2}) & 0x3F) << 6
+                    | (ord($c{$index + 3}) & 0x3F);
+            }
+            return false;
         } else {
             return mb_ord($char, $encoding);
         }
@@ -175,21 +182,14 @@ Class GenCoder
      * @return string
      * @throws Exception
      */
-    private function generateKey(){
+    private function generateKey()
+    {
         $key = "";
-        for($x = 0; $x<$this->key_size; $x++){
-            $key.= dechex(random_int(0, 15));
+        $k_count = count($this->key_symbols);
+        for ($x = 0; $x < $this->key_size; $x++) {
+            $key .= $this->key_symbols[mt_rand(0, $k_count)];
         }
         return $key;
-        /*
-         if(!function_exists("random_bytes")){
-             $bytes = '';
-             while (mb_strlen($bytes) < $this->key_size) {
-                 $bytes .= chr(mt_rand(0, 255));
-             }
-             return $bytes;
-         }
-        return random_bytes($this->key_size);*/
     }
 
     /**
@@ -199,14 +199,14 @@ Class GenCoder
      * @param $receiver_hashcode
      * @return string
      */
-    public function codeMessage($message, $generateKey, $user1_pass, $receiver_hashcode){
+    public function codeMessage($message, $generateKey, $user1_pass, $receiver_hashcode)
+    {
         $sender_hashcode = $this->sender_hashcode($user1_pass);
         $attach_key = $this->attachKey($message, $this->salt);
         $path_key_signature = $this->pathKeySignature($sender_hashcode, $receiver_hashcode, $attach_key);
-        $result_cypher = $this->cypher($path_key_signature, $message, $generateKey) .  $attach_key;
+        $result_cypher = $this->cypher($path_key_signature, $message, $generateKey) . $attach_key;
         $result_cypher = base64_encode($result_cypher);
-        $zipped_string = gzencode($result_cypher, 9);
-        return $zipped_string;
+        return gzencode($result_cypher, 9);
     }
 
     /**
@@ -215,51 +215,51 @@ Class GenCoder
      * @param $generateKey
      * @return string
      */
-    private function cypher($path_key_signature, $message, $generateKey){
-        $cyper_message = "";
+    private function cypher($path_key_signature, $message, $generateKey)
+    {
+        $cypher_message = "";
         $message = $this->str_splitUTF8($message);
         $sign_key = 0;
         $cur_key_pos = 0;
         $key_length = mb_strlen($generateKey, "UTF-8");
         for ($i = 0; $i < count($message); $i++) {
-            if($sign_key>=self::hash_length) $sign_key = 0;
+            if ($sign_key >= self::hash_length) $sign_key = 0;
             $key_code_pos = hexdec($path_key_signature[$sign_key]);
-            $cur_key_pos = $cur_key_pos+$key_code_pos;
-            if($cur_key_pos>=$key_length){
+            $cur_key_pos = $cur_key_pos + $key_code_pos;
+            if ($cur_key_pos >= $key_length) {
                 $cur_key_pos = $cur_key_pos - $key_length;
             }
-            //echo  $cur_key_pos . " " . $generateKey[$cur_key_pos] , "<br>";
-            //$key_symbol = hex2bin($generateKey[$cur_key_pos]);
             $key_symbol = $generateKey[$cur_key_pos];
-            $cyper_message .= $this->mb_chr($this->mb_ord($message{$i}) ^ $this->mb_ord($key_symbol));
+            $cypher_message .= $this->mb_chr($this->mb_ord($message{$i}) ^ $this->mb_ord($key_symbol));
             $sign_key++;
         }
-
-        return $cyper_message;
+        return $cypher_message;
     }
 
     /**
-     * @param $cyper
+     * @param $cypher
      * @param $generateKey
      * @param $user1_hashcode
      * @param $user2_pass
      * @return string
      */
-    public function decodeMessage($cyper, $generateKey, $user1_hashcode, $user2_pass){
-        $cyper = gzdecode($cyper);
-        $cyper = base64_decode($cyper);
+    public function decodeMessage($cypher, $generateKey, $user1_hashcode, $user2_pass)
+    {
+        $cypher = gzdecode($cypher);
+        $cypher = base64_decode($cypher);
         $user2_hashcode = $this->receiver_hashcode($user2_pass);
-        $attach_key_test = mb_substr($cyper, mb_strlen($cyper, "UTF-8")-self::hash_length, self::hash_length, "UTF-8");
+        $attach_key_test = mb_substr($cypher, mb_strlen($cypher, "UTF-8") - self::hash_length, self::hash_length, "UTF-8");
         $path_key_signature_test = $this->pathKeySignature($user1_hashcode, $user2_hashcode, $attach_key_test);
-        $trimmed_cyper = mb_substr($cyper, 0, mb_strlen($cyper, "UTF-8")-self::hash_length, "UTF-8");
-        return $this->cypher($path_key_signature_test, $trimmed_cyper, $generateKey);
+        $trimmed_cypher = mb_substr($cypher, 0, mb_strlen($cypher, "UTF-8") - self::hash_length, "UTF-8");
+        return $this->cypher($path_key_signature_test, $trimmed_cypher, $generateKey);
     }
 
     /**
      * @return array
      * @throws Exception
      */
-    public function init(){
+    public function init()
+    {
         $generateKey = $this->generateKey();
         $pass1 = $this->createPass();
         $pass2 = $this->createPass();
@@ -275,10 +275,11 @@ Class GenCoder
      * @return string
      * @throws Exception
      */
-    public function createPass(){
+    public function createPass()
+    {
         $pass = "";
-        for($t=0; $t<$this->pass_length; $t++){
-            $pass.= random_int(0, 9);
+        for ($t = 0; $t < $this->pass_length; $t++) {
+            $pass .= mt_rand(0, 9);
         }
         return $pass;
     }
--- index.php
+++ index.php
@@ -15,10 +15,16 @@ require_once("GenCoder.php");
 $message = $_POST["m"] ?? $text;
 // not for real project, requires constant string key
 
-$gen_coder = new GenCoder(md5("GenCode"));
+try {
+    $gen_coder = new GenCoder(md5("GenCode"));
+} catch (Exception $e) {
+}
 session_start();
 if (isset($_GET['reset']) OR !isset($_SESSION['key'])) {
-    $gen_params = $gen_coder->init();
+    try {
+        $gen_params = $gen_coder->init();
+    } catch (Exception $e) {
+    }
     $key = $gen_params['key'];
     $pass1 = $gen_params['pass1'];
     $pass2 = $gen_params['pass2'];
@@ -37,284 +43,13 @@ $receiver_hashcode = $gen_coder->receiver_hashcode($pass2);
 $coded_message = $gen_coder->codeMessage($message, $key, $pass1, $receiver_hashcode);
 
 ?>
-<html>
+<html lang="en">
 <head>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-    <style>
-        body, html {
-            font-family: 'Open Sans', sans-serif;
-            color: #666;
-            font-size: 14px;
-        }
-
-        h1 {
-            font-size: 27px;
-
-            color: #151010;
-
-            text-shadow: 0px 1px 1px #605555;
-
-            background: #4d4444;
-
-            border-radius: 10px;
-
-            padding: 14px;
-
-            border-top: 1px solid #0f0608;
-
-            border-bottom: 2px solid #131010;
-
-            box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.35);
-        }
-
-        h4 {
-            font-size: 16px;
-        }
-
-        hr {
-            border: 0px solid #000;
-            width: 95%;
-            background: #000;
-            height: 2px;
-            box-shadow: inset 0px 1px 10px 1px rgba(50, 50, 50, 0.26);
-            border-bottom: 1px solid #262626;
-            border-top: 1px solid #000;
-        }
-
-        textarea {
-            min-width: 550px;
-            min-height: 150px;
-            border: 1px solid #888;
-        }
-
-        .code_it {
-            display: block;
-            margin: 10px auto;
-            background: #eee;
-            padding: 6px;
-            font-size: 18px;
-            border-radius: 6px;
-            min-width: 200px;
-        }
-
-        textarea {
-            font-size: 16px;
-            color: #444;
-            padding: 10px;
-        }
-
-        .content {
-            text-align: left;
-        }
-
-        a.link {
-            text-decoration: none;
-            background: #eee;
-            padding: 5px;
-            color: #000;
-            margin: 10px;
-            font-size: 18px;
-        }
-
-        .ava {
-            background-position: center center;
-            background-repeat: no-repeat;
-            background-size: cover;
-            border-radius: 3px;
-            display: inline-block;
-            height: 50px;
-            width: 50px;
-            background-color: transparent;
-            background-image: url('/img/ava.png');
-        }
-
-        .gencoder {
-            color: #20bd00;
-            font-size: 36px;
-        }
-
-        a {
-            color: #a48989;
-        }
-        ul {
-            text-align: left;
-
-            color: #a48989;
-
-            font-size: 15px;
-
-            line-height: 30px;
-
-            letter-spacing: 0.6px;
-
-            font-style: italic;
-
-            text-shadow: 1px 1px 1px rgb(0, 0, 0);
-        }
-
-        .sh_php {
-            background: #f0f0f0 !important;
-            padding: 20px !important;
-            border: 1px solid #d9d9d9;
-            min-width: 700px;
-        }
-
-        .rand_sym {
-            background: #000;
-            color: red;
-            font-weight: bold;
-        }
-
-        /* Main styles */
-        @import url(https://fonts.googleapis.com/css?family=Open+Sans:800);
-        .text {
-            fill: none;
-            stroke-width: 3;
-            stroke-linejoin: round;
-            stroke-dasharray: 70 330;
-            stroke-dashoffset: 0;
-            -webkit-animation: stroke 12s infinite linear;
-            animation: stroke 12s infinite linear;
-        }
-
-        .text:nth-child(5n + 1) {
-            stroke: #F2385A;
-            -webkit-animation-delay: -2.4s;
-            animation-delay: -2.4s;
-        }
-
-        .text:nth-child(5n + 2) {
-            stroke: #F5A503;
-            -webkit-animation-delay: -4.8s;
-            animation-delay: -4.8s;
-        }
-
-        .text:nth-child(5n + 3) {
-            stroke: #000000;
-            -webkit-animation-delay: -7.2s;
-            animation-delay: -7.2s;
-        }
-
-        .text:nth-child(5n + 4) {
-            stroke: #56D9CD;
-            -webkit-animation-delay: -9.6s;
-            animation-delay: -9.6s;
-        }
-
-        .text:nth-child(5n + 5) {
-            stroke: #3AA1BF;
-            -webkit-animation-delay: -12s;
-            animation-delay: -12s;
-        }
-
-        @-webkit-keyframes stroke {
-            100% {
-                stroke-dashoffset: -400;
-            }
-        }
-
-        @keyframes stroke {
-            100% {
-                stroke-dashoffset: -400;
-            }
-        }
-
-        /* Other styles */
-        html, body {
-            height: 100%;
-        }
-
-        body {
-            background: #111;
-            margin: 0;
-            font-family: sans-serif;
-            color: #c5caaf;
-            font-size: 14px;
-        }
-
-        svg {
-            position: absolute;
-            width: 800px;
-            height: 120px;
-        }
-
-        .main_logo {
-            font-size: 100px;
-            width: 900px;
-            margin: auto;
-            margin-top: 10px;
-            height: 120px;
-        }
-
-        .container {
-            margin: auto;
-            width: 980px;
-            margin-top: 20px;
-            padding: 20px;
-            text-align: center;
-            border: 1px solid #151515;
-            background: #171717;
-            box-shadow: -6px -6px 16px 0px rgba(0, 0, 0, 0.5);
-        }
-
-        .step_element_icon {
-            width: 120px;
-            height: 120px;
-        }
-
-        .step_element_icon_mini {
-            width: 50px;
-            height: 50px;
-
-        }
-
-        .step_element_content {
-            margin-left: 50px;
-        }
-
-        .step_element_header {
-            margin-bottom: 20px;
-            background: #4d4444;
-            padding: 10px;
-            font-size: 20px;
-            text-align: left;
-            color: #211010;
-            line-height: 28px;
-            text-shadow: 0px 1px 1px #605555;
-        }
-
-        .step_element {
-            display: flex;
-            margin-bottom: 30px;
-        }
-
-        .style_colored {
-            color: #3083c9;
-            line-height: 50px;
-            margin-left: 25px;
-            margin-right: 25px;
-            font-size: 32px;
-        }
-
-        p {
-            padding: 20px;
-            line-height: 22px;
-            text-indent: 20px;
-        }
-
-        .flex_item {
-        }
-        .flex {
-            display: flex;
-            margin-left: 150px;
-            margin-bottom: 50px;
-        }
-
-    </style>
+    <link rel="stylesheet" type="text/css" href="style.css">
     <script src="http://shjs.sourceforge.net/sh_main.min.js"></script>
     <script src="http://shjs.sourceforge.net/lang/sh_php.js"></script>
     <link rel="stylesheet" href="http://shjs.sourceforge.net/sh_style.css" type="text/css">
-
 </head>
 <body onload="sh_highlightDocument();">
 
@@ -334,9 +69,7 @@ $coded_message = $gen_coder->codeMessage($message, $key, $pass1, $receiver_hashc
 </div>
 
 <div class="container">
-    <h1> Open source библиотека симметричного шифрования на PHP от <a class="link" href="https://bitbucket.org/astricus"
-                                                                      title="https://bitbucket.org/astricus"><span
-                    class="ava"></span></a>
+    <h1> Open source библиотека симметричного шифрования на PHP от <a class="link" href="https://bitbucket.org/astricus" title="https://bitbucket.org/astricus"><span class="ava"></span></a>
     </h1>
     <br><br>
 
@@ -402,8 +135,6 @@ $pass2 = $gen_params['pass2'];
 //закодированное сообщение
 $sender_hashcode = $gen_coder->sender_hashcode($pass1);
 $receiver_hashcode = $gen_coder->receiver_hashcode($pass2);
-
-$coded_message = $gen_coder->codeMessage($message, $key, $pass1, $receiver_hashcode);
 </pre>
 
         </div>
@@ -453,10 +184,6 @@ $coded_message = $gen_coder->codeMessage($message, $key, $pass1, $receiver_hashc
 //$key берется из хранилища у адресата
 //$sender_hashcode  - хеш (публичный ключ) отправителя
 //$pass2 - пароль адресата на дешифрование канала с данным отправителем
-$sender_hashcode = $gen_coder->sender_hashcode($pass1);
-$receiver_hashcode = $gen_coder->receiver_hashcode($pass2);
-
-$coded_message = $gen_coder->codeMessage($message, $key, $pass1, $receiver_hashcode);
 
 $plain_message = $gen_coder->decodeMessage($coded_message, $key, $sender_hashcode, $pass2);
 //$plain_message - исходная, дешифрованная информация
@@ -467,7 +194,7 @@ $plain_message = $gen_coder->decodeMessage($coded_message, $key, $sender_hashcod
 
     <br>
     <hr>
-    <h2><a name="randomize">Усиление защиты ключа путем рандомизации</a></h2>
+    <h2><a name="randomize">Усиление защиты ключа путем генерации функции обхода ключа (сигнатуры пути)</a></h2>
     <p class="content">Обычно при использовании метода одноразовых блокнотов или приближенных к нему алгоритмов сообщения в канале шифрованной связи
     шифруются каждый раз уникальным ключем из уникальных последовательностей случайных символов. При этом возникают следующие критические проблемы:<br>
     1. Необходимо заранее передавать уникальный случайный ключ отправита адресату для каждого шифросообщения.<br>
@@ -475,7 +202,7 @@ $plain_message = $gen_coder->decodeMessage($coded_message, $key, $sender_hashcod
 
     Рандомизация секретного ключа  позволяет избавиться сразу от этих двух проблем путем простого решения:<br>
     1. Исходный секретный ключ состоит из сгенерированных случайным образом символов создается разово и хранится у адресата и отправителя при создании шифрованного канала.
-    При этом секретный ключ достаточно малого размера, по умолчанию он составляет 64кб.<br>
+    При этом секретный ключ достаточно малого размера, по умолчанию он составляет от 64 до 100 символов.<br>
     2. При каждом новом шифровании сообщения отправителя на основе сложного алгоритма генерируется метод обхода исходного ключа, при этом метод зависит
     от хешей ключей адресата и отправителя, шифруемого сообщения и исходного ключа.<br>
     То есть, предметно говоря, если при шифровании методом Вернама первый символ шифросообщения получается операцией XOR первого символа исходного текста с первым символов ключа и так далее, пока не будут
